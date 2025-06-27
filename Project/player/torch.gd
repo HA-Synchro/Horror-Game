d5b018d4cca55b00a1aa16087675bfab5e7f2f14
Hashir Shahzad <hashir447079@gmail.com>
2025-06-27 11:16:54 +0500

    Fixed The toy's texture

--- Project/player/torch.gd
+++ Project/player/torch.gd
@@ -1,28 +1,29 @@
-extends Node3D
-class_name torch3d
-
-@onready var spot_light_3d: SpotLight3D = $"../SpotLight3D"
-var label = UIManager.get_node("CanvasLayer/Battery")
-
-@export var battery : int = 4
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+extends SpotLight3D
+class_name Torch3D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if battery > 0:
+var label : Label = UIManager.get_node("CanvasLayer/Battery")
+var can_use_torch : bool = true
+
+
+@export var battery : int = 4:
+	set(value):
+		battery = value
 		label.text = "Battery: " + str(battery)
-	else:
-		label.text = "Battery: 0" 
+
+func _ready() -> void:
+	battery = 4
+
+
+func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("torch"):
-		battery -= 1
-	if Input.is_action_pressed("torch") and battery > 0:
-		torch_on()
-	else:
-		spot_light_3d.visible = false
-	
-func torch_on():
-	spot_light_3d.visible = true
+		if !can_use_torch: return
+		if battery <= 0: return
+		can_use_torch = false
+		battery = battery - 1
+		light_energy = 3
+		var tween := get_tree().create_tween()
+		tween.tween_property(self, "light_energy", 0, 1)
+		await tween.finished
+		tween.kill()
+		can_use_torch = true
