96f6dbbbc025d57f945ce75f8acecbd648b377dd
Hashir Shahzad <hashir447079@gmail.com>
2025-06-30 12:51:12 +0500

    minor tweaks

--- Project/interactables/cupboard.gd
+++ Project/interactables/cupboard.gd
@@ -1,10 +1,16 @@
 extends Interactable3D
 
+func _ready() -> void:
+	if $"../Toy".get_child(0):
+		can_interact = false
+
+
 func interact():
 	if can_interact == true:
 		if !GameManager.player_ref: return
 		
 		var child : Node3D = GameManager.player_ref.l_hand.get_child(0)
+		if !child: return
 		var toy = child.toy_scene.instantiate()
 		$"../Toy".add_child(toy)
 		child.queue_free()
--- Project/interactables/interactable_toy.gd
+++ Project/interactables/interactable_toy.gd
@@ -1,13 +1,15 @@
 extends Interactable3D
 class_name InteractableToy
 
+signal toy_picked_up
+
 @export var toy_scene : PackedScene = load("res://toy/toy.tscn")
 
 func interact():
 	if can_interact == true:
 		if !GameManager.player_ref: return
 		
-		
+		toy_picked_up.emit()
 		var toy = toy_scene.instantiate()
 		GameManager.player_ref.l_hand.add_child(toy)
 		on_ray_cast_uncollide()
